7a88a7f582b235d77d80da5931fc2c69295995dc
aamasuno <alvaro.amasuno@gmail.com>
2023-01-26 09:25:28 +0100

    Add files via upload

--- gymprogress.py
+++ gymprogress.py
@@ -13,6 +13,8 @@ from deta import Deta
 import io
 import base64
 import pdfplumber
+import plotly.graph_objects as go
+
 
 deta = Deta("a0k3zvhe_oQqy7qtLHt2jykvZ3nu1VFaEQPY5kRRT")
 
@@ -64,7 +66,7 @@ if menu=="Subir PDF de Control de Medidas y Dieta":
             
             # Avisar al usuario de que el archivo se ha subido correctamente
             st.success('El archivo PDF se ha subido correctamente y se han extraido las medidas correspondientes.')
-            st.info("El archivo PDF estará disponible en el apartado 'Ver y Descargar PDF de Control Medidas y Dieta'")
+            #st.info("El archivo PDF estará disponible en el apartado 'Ver y Descargar PDF de Control Medidas y Dieta'")
             st.header("Vista Previa")
             st.write('La tabla generada del control de medidas es la siguiente:')
             st.table(df)
@@ -72,19 +74,66 @@ if menu=="Subir PDF de Control de Medidas y Dieta":
             # Avisar al usuario que no se ha encontrado archivo
             st.warning('No se ha encontrado archivo para subir.')
     
-elif menu == 'Ver y Descargar PDF de Control Medidas y Dieta':
+elif menu == 'Dashboard Control de Medidas':
     
-    st.title ('Ver y Descargar PDF de Control de Medidas')
-    # Aviso de visualización al usuario
-    st.info('Sólo podrás visualizar el PDF de Control de Medidas y Dieta vía web correctamente a través de ordenador.')
+    st.title('Dashboard Control de Medidas')
     
-    # Botón de descarga de PDF
-    st.download_button(label="Descargar PDF", 
-        data=docs.get(username+'.pdf').read(),
-        file_name=username+'.pdf',
-        mime='application/octet-stream')
+    # Llamar a deta para coger el CSV de medidas
+    csv=docs.get(username + '.csv')
     
-    # Visor web pdf
-    base64_pdf = base64.b64encode(docs.get(username+'.pdf').read()).decode('utf-8')
-    pdf_display = f'<embed src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf">'
-    st.markdown(pdf_display, unsafe_allow_html=True)
+    if csv is not None:
+        
+        # Cargar el archivo CSV y volcarlo en un DataFrame
+        csv_name= username + ".csv"
+        with open(csv_name, "wb+") as f:
+            f.write(csv.read())
+        df= pd.read_csv(csv_name,sep=";")
+        csv.close()
+    
+        # fig2 = go.Figure(go.Indicator(
+        #     mode = "number+delta",
+        #     value = df.iloc[-1,0],
+        #     title = {'text': df.columns[0]},
+        #     delta = {'reference':df.iloc[-2,0]},
+        #     domain = {'x': [0, 1], 'y': [0, 1]}
+        # ))
+        # st.plotly_chart(fig2)
+        
+        st.header('Medidas a fecha ' + str(df.iloc[-1,len(df.columns)-1]) )     
+        st.info('Las flechas indican el aumento o disminución de la medida respecto al período anterior: '+ str(df.iloc[-2,len(df.columns)-1]) +'.')
+        
+        fig=go.Figure()
+        k=0
+        for i in range(0,2):
+            for j in range(0,4):
+                fig.add_trace(go.Indicator(
+                    mode = "number+delta",
+                    value = df.iloc[-1,k],
+                    delta = {'reference':df.iloc[-2,k]},
+                    title = {'text':df.columns[k]},
+                    domain = {'row':i, 'column':j}
+                    ))
+                k = k+1
+        fig.update_layout(grid = {'rows': 2, 'columns': 4, 'pattern': "independent"})
+        
+        st.plotly_chart(fig,use_container_width=True)
+        
+    else:
+        st.error("No se ha encontrado datos de medidas. Por favor, sube el archivo correspondiente en el apartado 'Subir PDF de Control de Medidas y Dieta'.")
+
+# elif menu == 'Ver y Descargar PDF de Control Medidas y Dieta':
+    
+#     st.title ('Ver y Descargar PDF de Control de Medidas')
+#     # Aviso de visualización al usuario
+#     st.info('Sólo podrás visualizar el PDF de Control de Medidas y Dieta vía web correctamente a través de ordenador.')
+    
+#     # Botón de descarga de PDF
+#     st.download_button(label="Descargar PDF", 
+#         data=docs.get(username+'.pdf').read(),
+#         file_name=username+'.pdf',
+#         mime='application/octet-stream')
+    
+#     # Visor web pdf
+#     base64_pdf = base64.b64encode(docs.get(username+'.pdf').read()).decode('utf-8')
+#     pdf_display = f'<embed src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf">'
+#     st.markdown(pdf_display, unsafe_allow_html=True)
