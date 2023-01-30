a4e318948355ac53f25a7a3858673c52a5c64014
aamasuno <alvaro.amasuno@gmail.com>
2023-01-30 01:34:15 +0100

    Add files via upload

--- gymprogress.py
+++ gymprogress.py
@@ -15,6 +15,7 @@ import pdfplumber
 import plotly.graph_objects as go
 import datetime
 import numpy as np
+from rembg import remove
 
 st.set_page_config(layout="wide")
 
@@ -22,7 +23,8 @@ deta = Deta("a0k3zvhe_oQqy7qtLHt2jykvZ3nu1VFaEQPY5kRRT")
 
 username="Alvaro Amasuno"
 
-menu=st.sidebar.radio('Escoge una opción',['Subir PDF de Control de Medidas y Dieta','Subir Fotos','Dashboard Control de Medidas','Dashboard Fotos','Ver y Descargar PDF de Control Medidas y Dieta'])
+st.sidebar.title('Menú')
+menu=st.sidebar.radio('Escoge una opción',['Dashboard Control de Medidas','Dashboard Fotografías','Subir PDF de Control de Medidas y Dieta','Subir Fotografías'])
 
 # Abrir instancia DETA DRIVE para documentos
 docs = deta.Drive(username.replace(" ","_")+'_docs')
@@ -109,7 +111,7 @@ elif menu == 'Dashboard Control de Medidas':
         
         df=df[df[df.columns[-1]] >= np.datetime64(fini)]    
         
-        st.header('Medidas a fecha ' + df.iloc[-1,len(df.columns)-1].strftime("%d/%m/%Y")+'.')     
+        st.header('Medidas a fecha ' + df.iloc[-1,len(df.columns)-1].strftime("%d/%m/%Y"))     
         
         # Aviso solo un registro
         if len(df)==1:
@@ -135,7 +137,7 @@ elif menu == 'Dashboard Control de Medidas':
         
         st.plotly_chart(fig,use_container_width=True)
         
-        st.header('Diferencia de medidas acumulada a fecha de ' + df.iloc[-1,len(df.columns)-1].strftime("%d/%m/%Y")+'.')
+        st.header('Diferencia de medidas acumulada a fecha de ' + df.iloc[-1,len(df.columns)-1].strftime("%d/%m/%Y"))
         st.info('Las flechas indican el aumento o disminución de la medida respecto al primer registro del período de referencia: '+ df.iloc[0,len(df.columns)-1].strftime("%d/%m/%Y") +'.')
         
         # Aviso solo un registro
@@ -179,12 +181,12 @@ elif menu == 'Dashboard Control de Medidas':
                  data=docs.get(username+'.pdf').read(),
                  file_name=username+'.pdf',
                  mime='application/octet-stream')
-        st.subheader('Descargar histórico de datos de medidas')
+        st.subheader('Descargar histórico de medidas')
         st.download_button(label="Descargar histórico de medidas en CSV", 
                  data=df_all.to_csv(sep=";",index=False).encode('utf-8'),
                  file_name=username+'.csv',
                  mime='text/csv')
-        st.subheader('Descargar datos de medidas en el período de referencia')
+        st.subheader('Descargar medidas en el período de referencia')
         st.download_button(label="Descargar medidas del período de referencia en CSV", 
                  data=df.to_csv(sep=";",index=False).encode('utf-8'),
                  file_name=username+'_filtrado.csv',
@@ -192,19 +194,151 @@ elif menu == 'Dashboard Control de Medidas':
     else:
         st.error("No se ha encontrado datos de medidas. Por favor, sube el archivo correspondiente en el apartado 'Subir PDF de Control de Medidas y Dieta'.")
 
-# elif menu == 'Ver y Descargar PDF de Control Medidas y Dieta':
+elif menu == 'Subir Fotografías':
+    st.title ('Subir Fotografías')
+    st.info("Selecciona las fotografías a subir. Al mostrarse la vista previa, rellena los datos solicitados y presiona finalmente el botón 'Subir Fotografías'.")
+    giro=['No','90º a la izquierda', '90º a la derecha', '180º']
+    posicion=['Frontal','Frontal con flexión de bíceps','Dorsal','Dorsal con flexión de bíceps','Perfil con extensión de brazos']
+    postr=['frontal1','frontal2','dorsal1','dorsal2','perfil']
+    vimg=[]
+    vname=[]
+    vext=[]
     
-#     st.title ('Ver y Descargar PDF de Control de Medidas')
-#     # Aviso de visualización al usuario
-#     st.info('Sólo podrás visualizar el PDF de Control de Medidas y Dieta vía web correctamente a través de ordenador.')
+    uploaded_files = st.file_uploader("Elige las fotografias a subir",type=['png','jpg','jpeg'],accept_multiple_files=True)
+
+    if uploaded_files != []:
+        st.header("Vista Previa")
+        col1, col2 = st.columns(2)
+        k=1
+        for uploaded_file in uploaded_files:
+            st.subheader('Foto '+str(k))
+            col1, col2 = st.columns(2)
+            im = BytesIO(uploaded_file.read())
+            col1.image(im)
+            optgiro=col2.selectbox('¿Necesitas girar la imagen respecto a la vista actual?',options=giro,key="giro"+str(k))
+            optpos=col2.selectbox('Posición',options=posicion,key="pos"+str(k))
+            fecha = col2.date_input(label="Selecciona o escribe la fecha en que se ha tomado la fotografía (Fomato AAAA/MM/DD)",key="fecha"+str(k))
+            
+            if optgiro=='No':
+                vimg.append(Image.open(im))
+            elif optgiro == '90º a la izquierda':
+                vimg.append(Image.open(im).transpose(Image.Transpose.ROTATE_90))
+            elif optgiro == '90º a la derecha':
+                vimg.append(Image.open(im).transpose(Image.Transpose.ROTATE_270))
+            else:
+                vimg.append(Image.open(im).transpose(Image.Transpose.ROTATE_180))
+            
+            vext.append(uploaded_file.name.split(".")[-1])
+            vname.append(username+'_'+fecha.strftime("%Y%m%d")+'_'+postr[posicion.index(optpos)]+'.'+uploaded_file.name.split(".")[-1])
+            k=k+1
+        
+        if st.button("Subir fotografías"):
+            if uploaded_files is not None:
+                for i in range(0,len(vname)):
+                    buf = io.BytesIO()
+                    vimg[i].save(buf,format='PNG')
+                    fotos.put(vname[i],buf.getvalue())
+            
+                st.success("Las fotografías se han subido correctamente, podrás visualizarlas en el apartado 'Dashboard Fotos'.")
+
+elif menu=="Dashboard Fotografías":
     
-#     # Botón de descarga de PDF
-#     st.download_button(label="Descargar PDF", 
-#         data=docs.get(username+'.pdf').read(),
-#         file_name=username+'.pdf',
-#         mime='application/octet-stream')
     
-#     # Visor web pdf
-#     base64_pdf = base64.b64encode(docs.get(username+'.pdf').read()).decode('utf-8')
-#     pdf_display = f'<embed src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf">'
-#     st.markdown(pdf_display, unsafe_allow_html=True)
+    st.title("Dashboard Fotografías")
+    # Obtener lista de fechas de las fotografias subidas
+    lista=[item.split("_")[1] for item in fotos.list()["names"]]
+    posicion=['Frontal','Frontal con flexión de bíceps','Dorsal','Dorsal con flexión de bíceps','Perfil con extensión de brazos']
+    postr=['frontal1','frontal2','dorsal1','dorsal2','perfil']
+    
+    if len(lista)==0:
+        st.error('No se han encontrado fotografías que mostrar.')
+    else:
+        # Obtener lista de fechas en formato AAAAMMDD y DD/MM/AAAA
+        fechas_all = [*set(lista)]
+        fechas_all.sort()
+        fechas_all_fmt=[item[6:]+'/'+item[4:6]+'/'+item[:4] for item in fechas_all]
+        
+        st.header('Opción quitar el fondo')
+        st.info('Si deseas quitar el fondo a las fotografías, deja marcada esta opción. En caso contrario, desmarca la casilla que verás a continuación.')
+        sinfondo=st.checkbox("Quitar el fondo de las fotografías",value=True)
+        
+        st.header("Elegir el período de referencia")
+        
+        st.info('A continuación podrás configurar el inicio del período de referencia que quieras observar.\
+                Por defecto, se selecciona el día 1 del mes actual y año anterior a tu última fecha de revisión.')
+        st.warning('¡Cuidado! La fecha a elegir se mostrará en formato AAAA/MM/DD. Puedes elegirla desde el menú desplegable\
+                   o escribirla en el formato especificado directamente.')
+        
+        finiper = st.date_input(label="Escoge la fecha inicial del período de referencia",
+                             value=datetime.datetime(int(fechas_all[-1][:4])-1,int(fechas_all[-1][4:6]),1),
+                             max_value=datetime.datetime(int(fechas_all[-1][:4]),int(fechas_all[-1][4:6]),int(fechas_all[-1][6:])))
+        st.write(fechas_all[-1])
+        st.success('Tu período seleccionado es: '+finiper.strftime("%d/%m/%Y")+'-'+fechas_all_fmt[-1]+'.')
+        
+        fechas= [fechas_all[i] for i in range(0,len(fechas_all)) if finiper.strftime("%Y%m%d")<=fechas_all[i]]
+        fechas_fmt=[fechas_all_fmt[i] for i in range(0,len(fechas_all)) if finiper.strftime("%Y%m%d")<=fechas_all[i]]
+
+        st.header('Comparativa de fotografías')
+        if len(fechas)==1:
+            st.warning("Se ha encontrado solamente una fecha de registro. Se mostraran únicamente las fotografías de esa fecha")
+            for pos in postr:
+                st.subheader('Vista '+posicion[postr.index(pos)])
+                # No se porque pero se suben en formato .jpg en vez de .png
+                foto = fotos.get(username+'_'+fechas[0]+'_'+pos+'.jpg')
+                if foto is not None:
+                    nfoto = username+'_'+fechas[0]+'_'+pos+'.jpg'
+                    with open(nfoto, "wb+") as f:
+                        for chunk in foto.iter_chunks(4096):
+                              f.write(chunk)
+                    foto.close()
+                    col1, col2 = st.columns(2)
+                    col1.write(fechas_fmt[0])
+                    if sinfondo:
+                        col1.image(remove(Image.open(nfoto)))
+                    else:
+                        col1.image(nfoto)
+                else:
+                    st.error('No se ha encontrado la fotografia correspondiente a la posición en la fecha indicada.')
+        else:
+            col1, col2 = st.columns(2)
+            fini=col1.selectbox("Selecciona la fecha de inicio de la comparativa",fechas_fmt[:-1])
+            ffin=col2.selectbox("Selecciona la fecha de fin de la comparativa",fechas_fmt[fechas_fmt.index(fini)+1:],index=len(fechas_fmt[fechas_fmt.index(fini)+1:])-1)
+            for pos in postr:
+                st.subheader('Vista '+posicion[postr.index(pos)])
+                foto1 = fotos.get(username+'_'+fechas[fechas_fmt.index(fini)]+'_'+pos+'.jpg')
+                foto2 = fotos.get(username+'_'+fechas[fechas_fmt.index(ffin)]+'_'+pos+'.jpg')
+                col1, col2 = st.columns(2)
+                if foto1 is not None:
+                    nfoto = username+'_'+fechas[fechas_fmt.index(fini)]+'_'+pos+'.jpg'
+                    with open(nfoto, "wb+") as f:
+                        for chunk in foto1.iter_chunks(4096):
+                              f.write(chunk)
+                    foto1.close()
+                    col1.write(fini)
+                    if sinfondo:
+                        col1.image(remove(Image.open(nfoto)))
+                    else:
+                        col1.image(nfoto)
+                else:
+                    col1.write(fini)
+                    col1.error('No se ha encontrado la fotografía correspondiente a la posición en la fecha indicada.')
+                
+                if foto2 is not None:
+                    nfoto = username+'_'+fechas[fechas_fmt.index(ffin)]+'_'+pos+'.jpg'
+                    with open(nfoto, "wb+") as f:
+                        for chunk in foto2.iter_chunks(4096):
+                              f.write(chunk)
+                    foto2.close()
+                    col2.write(ffin)
+                    if sinfondo:
+                        col2.image(remove(Image.open(nfoto)))
+                    else:
+                        col2.image(nfoto)
+                else:
+                    col2.write(ffin)
+                    col2.error('No se ha encontrado la fotografía correspondiente a la posición en la fecha indicada.')
+    
+    
+    
+        
+    
